4c1f9dbaebd2a893c5ccad7500ca4058995ea697
Symeon Huang <hzwhuang@gmail.com>
2015-09-27 20:43:11 +0100

    adjust the structure according to kde techbase

--- mkplasmoid.sh
+++ mkplasmoid.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+cd plasmoid
 ver=`grep "X-KDE-PluginInfo-Version" metadata.desktop | sed 's/.*=//'`
 app=`grep "X-KDE-PluginInfo-Name" metadata.desktop | sed 's/.*=//'`
 zip ~/$app-$ver.plasmoid -r contents
-zip ~/$app-$ver.plasmoid metadata.desktop LICENSE README.md
+zip ~/$app-$ver.plasmoid metadata.desktop
+cd ..
+zip ~/$app-$ver.plasmoid LICENSE README.md
